13d717b5dd075c765f138a045eb74a0562e17e70
b2 <brendan@mobilewebsmart.com>
2020-04-18 16:05:00 -0600

    Calculator api controller to get viewmodel

---
+++ DotNetMVCReact/Controllers/CalculatorController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DotNetMVCReact.ViewModels;
+using DotNetMVCReact.Enums;
+
+namespace DotNetMVCReact.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CalculatorController : Controller
+    {
+        [HttpGet]
+        public JsonResult Get()
+        {
+            CalculatorViewModel calculatorViewModel = new CalculatorViewModel()
+            {
+                Categories = Enum.GetNames(typeof(CategoryEnum)).ToArray()
+            };
+
+            return Json(calculatorViewModel);
+        }
+    }
+}
