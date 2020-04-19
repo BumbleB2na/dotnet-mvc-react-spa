a6c49c790e184143f4b0bf0158404db7061cc551
b2 <brendan@mobilewebsmart.com>
2020-04-19 06:26:12 -0600

    Item data model

--- ViewModels/CalculatorViewModel.cs
+++ ViewModels/CalculatorViewModel.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DotNetMVCReact.Models;
 
 namespace DotNetMVCReact.ViewModels
 {
     public class CalculatorViewModel
     {
         public string[] Categories { get; set; }
+
+        public ICollection<Item> Items { get; set; }
     }
 }
