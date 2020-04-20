eba832693d611a868c4f1357796a9dc03fa00248
b2 <brendan@mobilewebsmart.com>
2020-04-20 07:37:05 -0600

    Refactored category section

--- ClientApp/src/components/ItemCategories.js
+++ ClientApp/src/components/ItemCategories.js
@@ -5,28 +5,14 @@ import ItemRow from './ItemRow';
 export default class ItemCategories extends Component {
     constructor(props) {
 		super(props);
-    }
+	}
 	
 	render() {
-		const categories = this.props.categories.slice()
-			.sort((a, b) => a !== b ? a < b ? -1 : 1 : 0);
-		const itemsByCategory = new Array(categories.length);
-		categories.forEach((category, index) => {
-			const catItems = this.props.items.slice()
-				.filter(item => item.category === category)
-				.sort((a, b) => a.name !== b.name ? a.name < b.name ? -1 : 1 : 0);
-			const totalValue = catItems.reduce((a, b) => a + b.value, 0);
-			itemsByCategory[index] = {
-				"category": category,
-				"totalValue": totalValue,
-				"items": catItems
-			}
-		});
-
-		const contents = itemsByCategory.map(category => {
+		const categoriesOfItems = this.getCategoriesOfItems();
+		const contents = categoriesOfItems.map(category => {
 			return (
 				<Container>
-					<ItemRow id={category.category} name={category.category} value={category.totalValue} onDeleteItem={false} />
+					<ItemRow id={category.name} name={category.name} value={category.totalValue} onDeleteItem={false} />
 					{category.items.map(item => {
 						return (
 							<ItemRow id={item.id} name={item.name} value={item.value} onDeleteItem={this.props.onDeleteItem} />
@@ -40,6 +26,30 @@ export default class ItemCategories extends Component {
 				{contents}
 			</div>
 		);
+	}
+
+	getCategoriesOfItems() {
+		const categories = this.props.categories.slice()
+			.sort(this.sortAlphabetically);
+		const categoriesOfItems = new Array(categories.length);
+		categories.forEach((category, index) => {
+			const catItems = this.props.items.slice()
+				.filter(item => item.category === category)
+				.sort(this.sortAlphabeticallyByName);
+			const totalValue = catItems.reduce((a, b) => a + b.value, 0);
+			categoriesOfItems[index] = {
+				"name": category,
+				"totalValue": totalValue,
+				"items": catItems
+			}
+		});
+		return categoriesOfItems;
+	}
+	sortAlphabetically(a, b) {
+		return a !== b ? a < b ? -1 : 1 : 0;
+	}
+	sortAlphabeticallyByName(a, b) {
+		return a.name || a !== b.name || b ? a.name || a < b.name || b ? -1 : 1 : 0;
 	}
 
 }
